ac951c6dd125608bcbac1766c9d5514a4dc3a156
tianzhich <zhi.tian@yahoo.com>
2019-11-11 17:34:00 +0800

    refactor: types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 /**
  * @Date: 2019-10-11 17:41:36
  * @LastEditors: Tian Zhi
- * @LastEditTime: 2019-11-04 20:56:23
+ * @LastEditTime: 2019-11-11 17:25:21
  */
 import React from "react";
 import "./App.css";
 import { PDFViewer } from "./PDFViewer";
 // import { PDF_FILE } from "./PDFViewer/const";
+// @ts-ignore
 import LONG_PDF from './assets/long-pdf.pdf';
 import { PDF_FILE } from "./PDFViewer/const";
 const DOC_URL = "https://pdfobject.com/pdf/sample-3pp.pdf";
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+/*
+ * @Date: 2019-11-11 17:19:08
+ * @LastEditors: Tian Zhi
+ * @LastEditTime: 2019-11-11 17:21:15
+ */
+declare module '*.pdf';
